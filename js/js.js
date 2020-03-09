602b1850486e6ce607f3e65c16fdc151674ca08c
agungpurnomo <agung.laravel@gmail.com>
2020-03-09 16:54:00 +0700

    repiar

--- js/js.js
+++ js/js.js
@@ -1,3 +1,8 @@
-function contact() {
-    alert('No Hp : 085743467242')
+function detailProfile() {
+    var detail=document.getElementById('detail_id');
+    if(detail.style.display=='none')
+    	document.getElementById('detail_id').style.display = "block"
+    else
+    	document.getElementById('detail_id').style.display = "none"
+    
 }
